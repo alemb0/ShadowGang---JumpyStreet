1c14de6d8d56373b187b3dec65c06c676b993e7f
gwendolynburkey <49504325+gwendolynburkey@users.noreply.github.com>
2020-09-28 15:21:03 -0400

    Add files via upload

---
+++ AIMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    [SerializeField] private bool facingLeft;
+    [SerializeField] private bool facingRight;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (facingLeft)
+        {
+            transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
+        }
+        else if (facingRight)
+        {
+            transform.Translate(new Vector3(0, 1, 0) * -speed * Time.deltaTime);
+        }
+
+        if (transform.position.z < -30)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (transform.position.z > 30)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
